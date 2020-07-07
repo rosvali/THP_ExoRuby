8d2de73c6ff0f7da9377593b4d9c6bbca33b4d9b
rosvali <rosval6578@gmail.com>
2020-07-07 21:40:04 +0200

    last modif

--- exo_19.rb
+++ exo_19.rb
@@ -1,6 +1,2 @@
-tab = (0..49).to_a
-tab.map do |i|
-	tab[i] = "jean.dupont.#{sprintf("%02d", i + 1)}@email.fr"
-	puts tab[i - 1] if (i % 2 == 0) && (i != 0)
-end
-puts tab[49]
+tab = (0..50).to_a
+tab.map! { |i| puts "jean.dupont.#{sprintf("%02d", i + 1)}@email.fr" if (i + 1).even? }
