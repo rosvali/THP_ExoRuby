a5d868623e5fed3664c829645b5129b04a3b4cbc
rosvali <rosval6578@gmail.com>
2020-07-07 22:11:58 +0200

    laaaast

--- exo_19.rb
+++ exo_19.rb
@@ -1,2 +1,4 @@
 tab = (0..50).to_a
-tab.map! { |i| puts "jean.dupont.#{sprintf("%02d", i + 1)}@email.fr" if (i + 1).even? }
+tab.map! { |i| "jean.dupont.#{sprintf("%02d", i + 1)}@email.fr" if (i + 1).even? }
+tab.compact!
+puts tab
